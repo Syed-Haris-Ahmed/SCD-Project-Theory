c1cca8b3f536d64324c7161044bf830a6754ea82
AbbasAbid10 <bcsbs2112316@szabist.pk>
2023-12-17 18:26:40 +0500

    modified

--- Cyber Warfare/resources/views/BorderUser.blade.php
+++ Cyber Warfare/resources/views/BorderUser.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="{{asset('css/BorderUser.css')}}">
     <title>Document</title>
 </head>
 <body>
@@ -14,9 +14,9 @@
                     <h1>CW</h1>
                 </div>
                 <div class="details">
-                    <img src="profile_icon.png" alt="Profile Icon" width="50px" height="50px">
+                    <img src="{{asset('res/profile_icon.png')}}" alt="Profile Icon" width="50px" height="50px">
                     <h4 style="margin-left: 15px; margin-right: 15px;">Syed Haris Ahmed<br><span style="color: lime; ">Border Resident</span></h4>
-                    <img src="drop_down_icon.png" alt="DrownDropIcon" width="40px" height="40px">
+                    <img src="{{asset('res/drop_down_icon.png')}}" alt="DrownDropIcon" width="40px" height="40px">
                 </div>
                 <div class="side-bar-options-container">
                     <div class="side-bar-option-div">
@@ -30,7 +30,7 @@
                     </div> -->
                 </div>
                 <div class="log-out-div">
-                    <img src="logout_icon.png" alt="Logout_Icon" width="50px" height="50px">
+                    <img src="{{asset('res/logout_icon.png')}}" alt="Logout_Icon" width="50px" height="50px">
                     <h1 style="margin-left: 20px;">Logout</h1>
                 </div>
             </div>
