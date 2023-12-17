977f81150337f497d2390251cffb4c94cdf10b33
Syed-Haris-Ahmed <bcsbs2112333@szabist.pk>
2023-12-17 17:18:18 +0500

    modified

--- Cyber Warfare/app/Http/Controllers/RegisterController.php
+++ Cyber Warfare/app/Http/Controllers/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 class RegisterController extends Controller
@@ -13,39 +12,38 @@ class RegisterController extends Controller
     }
 
     public function storeData(Request $request){
+        $validatedData = $request->validate([
+            'username' => 'required|string|max:100|unique:users,username',
+            'cnic' => 'required|digits:13|unique:users,cnic',
+            'password' => 'required|string|min:6',
+            'email' => 'required|email|unique:users,email',
+            'locations' => 'nullable|string',
+        ]);
 
-            $validatedData = $request->validate([
-                'username' => 'required|string|max:100|unique:users,username',
-                'cnic' => 'required|digits:13|unique:users,cnic',
-                'password' => 'required|string|min:6',
-                'email' => 'required|email|unique:users,email',
-                'locations' => 'nullable|string',
-            ]);
+        error_log(print_r($validatedData, true));
+        
+        if($request->has('border_resident')){
+            error_log("Error Log -> Inside IF Statement");
+            $validatedData['roleid']=2; 
 
-            error_log(print_r($validatedData, true));
-            
-            if($request->has('border_resident')){
-                error_log("Error Log -> Inside IF Statement");
-                $validatedData['roleid']=2; 
+        } else {
+            error_log("Erro Log -> Inside ELSE Statement");
+            $validatedData['roleid']=1;
+            $validatedData['locations']=null;
+        }
 
-            } else {
-                error_log("Erro Log -> Inside ELSE Statement");
-                $validatedData['roleid']=1;
-                $validatedData['locations']=null;
-            }
+        error_log(print_r($validatedData, true));
 
-            error_log(print_r($validatedData, true));
+        // $user = new User([
+        //     'username' => $validatedData['username'],
+        //     'password' => $validatedData['password'],
+        //     'cnic'     => $validatedData['cnic'],
+        //     'email' => $validatedData['email'],
+        //     'roleid' => $validatedData['roleid'] , 
+        //     'locations' => $validatedData['locations'] ?? null, 
+        // ]);
 
-            $user = new User([
-                'username' => $validatedData['username'],
-                'password' => $validatedData['password'],
-                'cnic'     => $validatedData['cnic'],
-                'email' => $validatedData['email'],
-                'roleid' => $validatedData['roleid'] , 
-                'locations' => $validatedData['locations'] ?? null, 
-            ]);
-
-            $user->save();
+        $user = User::create($validatedData);
 
         return redirect()->route('home');
     }
